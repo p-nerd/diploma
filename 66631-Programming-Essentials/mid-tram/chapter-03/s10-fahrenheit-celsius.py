447b48e22724e42e053f8894b039c99759502dd4
Shihab Mahamud <shihab4t@gmail.com>
2022-06-29 00:45:52 +0600

    mid

---
+++ 66631-Programming-Essentials/mid-tram/chapter-03/s10-fahrenheit-celsius.py
@@ -0,0 +1,3 @@
+fahrenheit = float(input("Please enter the fahrenheit temperature: "))
+celsius = (fahrenheit - 32) / 1.8
+print("%0.1f degree fahrenheit is = %0.1f degree celsius" % (fahrenheit, celsius))
