61d51f363118a08adbaaa364056c535a6ddf6203
Shihab Mahamud <shihab4t@gmail.com>
2022-11-28 14:06:52 +0600

    C#

---
+++ 66641-Object-Oriented-Programming/01HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World");
+            Console.WriteLine("I am a student");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 66641-Object-Oriented-Programming/02FindAreaOfTriangle/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FindAreaOfTriangle
+{
+    public class Program
+    {
+        static float findArea(float a, float b, float c)
+        {
+            if (a < 0 || b < 0 || c < 0 ||
+                a + b <= c || a + c <= b || b + c <= a)
+            {
+                Console.WriteLine("Not a valid triangle");
+                System.Environment.Exit(0);
+            }
+            float s = (a + b + c) / 2;
+            return (float)Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+        static void Main(string[] args)
+        {
+            float a = 3.0f;
+            float b = 4.0f;
+            float c = 5.0f;
+
+            Console.WriteLine("Area is " + findArea(a, b, c));
+        }
+    }
+}
