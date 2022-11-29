76367212e28f924bcabfc23c75120b709d68b8ac
Shihab Mahamud <shihab4t@gmail.com>
2022-11-29 10:54:09 +0600

    Derived

---
+++ 66641-Object-Oriented-Programming/04Derived/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Derived
+{
+    class Animal
+    {
+        public string name = "Something";
+
+        public void display()
+        {
+            Console.WriteLine("I am an animal");
+        }
+    }
+    class Dog : Animal
+    {
+        public void getName()
+        {
+            Console.WriteLine("My name is " + name);
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.display();
+        }
+    }
+}
+
